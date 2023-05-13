90d9b29a2993a1c73caa5ec4ae3dbfe69a3f2369
blthai <39754216+blthai@users.noreply.github.com>
2023-05-12 17:53:13 -0700

    add ability to display and add books in the table

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
-const table = document.getElementsByClassName("book-list")[0];
 const newBookButton = document.getElementsByClassName("new-book-button")[0];
 const newBookForm = document.getElementsByClassName("new-book-form")[0];
-
-let myLibrary = [];
+const bookList = document.getElementsByClassName("book-list")[0];
+const table = document.getElementsByClassName("library-contents")[0];
+const myLibrary = [];
+const existingBooks = [];
+let currentForm;
+let rowCount = 0;
 
 function Book(title, author, pageCount, readStatus) {
   this.title = title;
@@ -11,42 +14,56 @@ function Book(title, author, pageCount, readStatus) {
   this.readStatus = readStatus;
 }
 
-function addBookToLibrary() {
-  const book = prompt("Add a new book!");
-  myLibrary.push(book);
-}
-
 function updateLibrary() {
-  let rowCount = 0;
   myLibrary.forEach((book) => {
-    const newRow = table.insertRow(rowCount);
-    let cellCount = 0;
-    Object.values(book).forEach((prop) => {
-      const newCell = newRow.insertCell(cellCount);
-      const newCellText = document.createTextNode(prop);
-      newCell.appendChild(newCellText);
-      cellCount += 1;
-    });
-    rowCount += 1;
+    if(!(existingBooks.includes(book.title))){
+      const newRow = bookList.insertRow(rowCount);
+      let cellCount = 0;
+      Object.values(book).forEach((prop) => {
+        const newCell = newRow.insertCell(cellCount);
+        const newCellText = document.createTextNode(prop);
+        newCell.appendChild(newCellText);
+        cellCount += 1;
+      });
+      
+      rowCount += 1;
+      existingBooks.push(book.title);
+    }
   });
 }
 
+function addBookToLibrary(book) {
+  event.preventDefault();
+  myLibrary.push(book);
+  updateLibrary();
+}
+
 function getNewBookInfo() {
-  const newForm = document.createElement('form');
+  currentForm = document.createElement('form');
   const titleInput = document.createElement('input');
+  titleInput.name = 'title';
   const authorInput = document.createElement('input');
   const pageCountInput = document.createElement('input');
   const readStatusInput = document.createElement('input');
   const submitButton = document.createElement('button');
   submitButton.type = 'submit';
   submitButton.appendChild(document.createTextNode('Add Book'))
-  newForm.appendChild(titleInput);
-  newForm.appendChild(authorInput);
-  newForm.appendChild(pageCountInput);
-  newForm.appendChild(readStatusInput);
-  newForm.appendChild(submitButton);
-  newBookForm.appendChild(newForm);
+  currentForm.appendChild(titleInput);
+  currentForm.appendChild(authorInput);
+  currentForm.appendChild(pageCountInput);
+  currentForm.appendChild(readStatusInput);
+  currentForm.appendChild(submitButton);
+  newBookForm.appendChild(currentForm);
+  currentForm.addEventListener('submit', () => {
+    addBookToLibrary(new Book(titleInput.value, authorInput.value, pageCountInput.value, readStatusInput.value));
+    titleInput.value="";
+    authorInput.value="";
+    pageCountInput.value="";
+    readStatusInput.value="";
+  })
 }
+
+newBookButton.addEventListener('click', getNewBookInfo, {once: true});
 
 const book1 = new Book(
   "'The Haunting of the Mansion on the Hill'",
@@ -69,10 +86,13 @@ const book3 = new Book(
   "Not Read"
 );
 
-newBookButton.addEventListener('click', getNewBookInfo)
 
 myLibrary.push(book1);
 myLibrary.push(book2);
 myLibrary.push(book3);
+existingBooks.push(book1);
+existingBooks.push(book2);
+existingBooks.push(book3);
 updateLibrary();
-console.log(myLibrary);
+
+
